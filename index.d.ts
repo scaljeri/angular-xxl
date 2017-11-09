22915a34db3abc0ff55239ef549eb39a7ad043b8
lucascalje <lucas@calje.eu>
2017-11-09 08:55:50 +0100

    Cleanup

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-import { RouteXxlConfig } from "./bundle/route-decorators";
-export { RouteXxlConfig };
-export declare const RouteData: (annotation?: string, config?: RouteXxlConfig) => any;
-export declare const RouteParams: (annotation?: string, config?: RouteXxlConfig) => any;
-export declare const RouteQueryParams: (annotation?: string, config?: RouteXxlConfig) => any;
--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var route_decorators_1 = require("./bundle/route-decorators");
-exports.RouteData = route_decorators_1.routeDecoratorFactory('data');
-exports.RouteParams = route_decorators_1.routeDecoratorFactory('params');
-exports.RouteQueryParams = route_decorators_1.routeDecoratorFactory('queryParams');
