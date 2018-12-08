58e027e73eb3e49c22ef1bc839c0d1e38d580ef7
Lucas Calje <lucas@calje.eu>
2018-12-08 14:59:31 +0100

    chore: Updated devDependencies, v1.0.2 with angular 7 peer dep

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,13 +1,17 @@
 module.exports = function(config) {
   config.set({
-    files: ['test/*.ts'],
-    testRunner: "mocha",
+    files: ['test/*.ts', 'src/*.ts'],
     mutator: "typescript",
-    transpilers: ["typescript"],
-    reporter: ["html", "clear-text", "progress"],
+    packageManager: "yarn",
+    reporters: ["html", "clear-text", "progress"],
+    testRunner: "mocha",
+    transpilers: ['babel'],
     testFramework: "mocha",
     coverageAnalysis: "off",
     tsconfigFile: "tsconfig.json",
-    mutate: ["src/*.ts"]
+    mutate: ["src/*.ts"],
+    mochaOptions: {
+        files: ['test/*.ts']
+    }
   });
 };
