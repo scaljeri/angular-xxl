007104f68c51238c5ff1025fd45a3e2713df4ea6
Lucas Calje <lucas@calje.eu>
2021-01-03 20:02:44 +0100

    fix: Angular 11 Ivy changed

--- test/replace-ng-oninit.spec.ts
+++ test/replace-ng-oninit.spec.ts
@@ -1,5 +1,5 @@
 import * as sinon from "sinon";
-import { NG_ON_INIT, IVY_ON_INIT, replaceNgOnInit, IVY_PROTOTYPE } from '../src/replace-ng-oninit';
+import { NG_ON_INIT, replaceNgOnInit } from '../src/replace-ng-oninit';
 
 export function replaceNgOnInitSpec(should): void {
     describe("replaceNgOnInit", () => {
@@ -34,35 +34,6 @@ export function replaceNgOnInitSpec(should): void {
 
             it('should call the original ngOnInit', () => {
                 ngOnInit.should.have.been.calledOnce;
-            });
-        });
-
-        describe('With Ivy', () => {
-            let ivyProto;
-
-            beforeEach(() => {
-                ivyNgOnInit = sinon.spy();
-                ivyProto = { [IVY_ON_INIT]: ivyNgOnInit };
-
-                proto.constructor = {
-                    ...proto,
-                    [IVY_PROTOTYPE]: ivyProto
-                };
-
-                replaceNgOnInit(proto, callback)
-                ivyProto[IVY_ON_INIT]();
-            });
-
-            it('should replace ngOnInit', () => {
-                ivyProto[IVY_ON_INIT].should.not.equal(ivyNgOnInit);
-            });
-
-            it('should call the decorator\'s ngOnInit', () => {
-                callback.should.have.been.calledOnce;
-            });
-
-            it('should call the original ngOnInit', () => {
-                ivyNgOnInit.should.have.been.calledOnce;
             });
         });
     });
